c356ebf7269214e5891780983dc5616e5a6c7d66
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-07-19 21:43:45 -0300

    Chess-System - 2023-07-19

--- Chess-System/src/boardgame/BoardException.java
+++ Chess-System/src/boardgame/BoardException.java
@@ -1,12 +1,10 @@
 package boardgame;
 
-import boardgame.BoardException;
+public class BoardException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 
-public class BoardException extends BoardException {
-    private static final long serialVersionUID = 1L;
-
-    public BoardException(String msg){
-        super(msg);
-    }
+	public BoardException(String msg) {
+		super(msg);
+	}
 }
 
--- Chess-System/src/chess/ChessPiece.java
+++ Chess-System/src/chess/ChessPiece.java
@@ -4,37 +4,38 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
-    private Color color;
-    private int moveCount;
+public abstract class ChessPiece extends Piece {
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+	private Color color;
+	private int moveCount;
 
-    public Color getColor() {
-        return color;
-    }
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-    public int getMoveCount(){
-        return moveCount;
-    }
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
 
-    public void increaseMoveCount(){
-        moveCount++;
-    }
+	protected void decreaseMoveCount() {
+		moveCount--;
+	}
 
-    public void decreaseMoveCount(){
-        moveCount--;
-    }
- 
-    public ChessPosition getChessPosition(){
-        return ChessPosition.fromPosition(position);
-    }
-
-    protected boolean isThereOpponentPiece(Position position){
-        ChessPiece p = (ChessPiece) getBoard().piece(position);
-        return p != null && p.getcolor() != color;
-    }
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
