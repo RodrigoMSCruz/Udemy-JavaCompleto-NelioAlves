130fcf949cc7cacb282de3ff3c2ac0fedb619111
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-08-07 15:24:16 -0300

    Seção 23 - Projeto Spring - User entity and resource

---
+++ 23/course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "123456");
+        return ResponseEntity.ok().body(u);
+    }
+}
