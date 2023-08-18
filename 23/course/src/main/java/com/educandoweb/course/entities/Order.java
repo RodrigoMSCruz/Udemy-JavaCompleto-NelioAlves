d542b7994a112d63a4d0f9d3dded65e7d24381a7
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-08-18 15:23:45 -0300

    Seção 23 - Subtotal & total

--- 23/course/src/main/java/com/educandoweb/course/entities/Order.java
+++ 23/course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -106,6 +106,13 @@ public class Order implements Serializable{
         this.items = items;
     }
 
+    public Double getTotal(){
+        double sum = 0.0;
+        for (OrderItem x : items){
+            sum = sum + x.getSubTotal();
+        }
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
