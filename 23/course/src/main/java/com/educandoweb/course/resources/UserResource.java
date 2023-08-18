726c91cea1c7b8420f3af5b32a8e3be7cf0cb96c
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-08-18 15:59:17 -0300

    Seção 23 - User Insert

--- 23/course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ 23/course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,10 @@ import com.educandoweb.course.services.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,5 +34,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> insert (@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created().body(uri).body(obj);
     }
 }
